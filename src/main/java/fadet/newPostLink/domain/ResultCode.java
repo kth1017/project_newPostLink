23700b6ef09c7a7edaf271ea98729b50d1625d5b
kth <rnjsxo1017@gmail.com>
2023-03-07 20:54:29 +0900

    ver 1.0.1

--- src/main/java/fadet/newPostLink/domain/ResultCode.java
+++ src/main/java/fadet/newPostLink/domain/ResultCode.java
@@ -19,36 +19,53 @@ public class ResultCode {
     public void init(String allCode, String titleHtmlKeyword, String indexHtmlKeyword, List<String> titleList) {
         // 기능1 타이틀별 링크
 
+        // 1 타이틀 키워드 기준으로 기존 코드를 나누는 스트링 배열 생성
         String[] oldCodeArrByKeyword = allCode.split(titleHtmlKeyword);
-        String addTitleText = oldCodeArrByKeyword[0];
-
+        // 2 생성한 배열을 활용해 수정 내용이 담길 빌더 작성
+        StringBuilder addTitleTextBuilder = new StringBuilder();
+        // 3 빌더에 기존 코드 배열의 머리말을 추가
+        addTitleTextBuilder.append(oldCodeArrByKeyword[0]);
+        // 4 타이틀 키워드를 공백 기준으로 나누는 스트링 배열 생성
         String[] modiTitleKeywordArr = titleHtmlKeyword.split(" ");
-
+        // 5 빌더에 링크가 수정된 코드로 채워넣음
         int arrLen = oldCodeArrByKeyword.length;
         for (int i = 1; i < arrLen; i++) {
-            addTitleText += modiTitleKeywordArr[0] + " id=\""+i+"\" "+modiTitleKeywordArr[1]+" "+modiTitleKeywordArr[2];
-            addTitleText += oldCodeArrByKeyword[i];
+            addTitleTextBuilder.append(modiTitleKeywordArr[0])
+                    .append(" id=\"").append(i).append("\" ")
+                    .append(modiTitleKeywordArr[1]).append(" ").append(modiTitleKeywordArr[2]);
+            addTitleTextBuilder.append(oldCodeArrByKeyword[i]);
         }
 
         // 기능2 인덱스 자동완성
 
+        // 1 빌더.toString
+        String addTitleText = addTitleTextBuilder.toString();
+
+        // 2 인덱스 키워드 기준으로 배열 생성
 //        String[] resultArr = addTitleText.split("<blockquote data-ke-style=\"style2\">Index</blockquote>");
         String[] resultArr = addTitleText.split(indexHtmlKeyword);
 //        String resultText = resultArr[0]+"<blockquote data-ke-style=\"style2\">Index";
 
-        String[] modiIndexKeywordArr = indexHtmlKeyword.split("<");
+        // 3 인덱스 키워드에 포함된 <를 기준으로 나누는 배열 생성
+        String[] modifiedIndexKeywordArr = indexHtmlKeyword.split("<");
 
-        String modiIndexKeyword = "<"+modiIndexKeywordArr[0]+modiIndexKeywordArr[1];
+        // 4 인덱스 키워드
+        String modiIndexKeyword = "<"+modifiedIndexKeywordArr[0]+modifiedIndexKeywordArr[1];
 
-        String resultText = resultArr[0]+modiIndexKeyword;
+        // 5 생성한 배열을 활용해 수정 내용이 담길 빌더 작성
+        StringBuilder resultTextBuilder = new StringBuilder();
+        resultTextBuilder.append(resultArr[0]+modiIndexKeyword);
 
         for (int i = 0; i < titleList.size(); i++) {
             int j = i+1;
-            resultText += "<br /><a href=\"#"+j+"\">"+j+" "+titleList.get(i)+"</a>";
+            resultTextBuilder.append("<br /><a href=\"#")
+                    .append(j).append("\">").append(j).append(" ")
+                    .append(titleList.get(i)).append("</a>");
         }
-        resultText += "<"+modiIndexKeywordArr[2]+resultArr[1];
+        resultTextBuilder.append("<").append(modifiedIndexKeywordArr[2]).append(resultArr[1]);
 
-        this.setResultAllCode(resultText);
+
+        this.setResultAllCode(resultTextBuilder.toString());
 
     }
 }
